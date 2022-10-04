f909bc1a9d47c64525a32e1674eed0a1f35ffbd2
JBBianchi <jb.bianchi@neuroglia.io>
2022-10-04 17:40:08 +0200

    Add project files.

---
+++ tests/Synapse.Demo.Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Synapse.Demo.Application.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/Synapse.Demo.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Synapse.Demo.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
