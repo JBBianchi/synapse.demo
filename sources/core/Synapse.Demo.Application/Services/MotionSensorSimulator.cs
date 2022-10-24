e6ee07af337fea91fb74e6cc323168ec09d7721b
Charles d'Avernas <charles.davernas@neuroglia.io>
2022-10-24 16:07:18 +0200

    fix(Application): Revamped the various simulator notification handlers into dedicated services and domain event handler

--- sources/core/Synapse.Demo.Application/Services/MotionSensorSimulator.cs
+++ sources/core/Synapse.Demo.Application/Services/MotionSensorSimulator.cs
@@ -1,19 +1,26 @@
-﻿using Neuroglia.Mediation;
-using Synapse.Demo.Application.Commands.Devices;
-using static System.Formats.Asn1.AsnWriter;
+﻿using Synapse.Demo.Application.Commands.Devices;
+using System.Collections.Concurrent;
 
 namespace Synapse.Demo.Application.Services;
 
 /// <summary>
-/// The service used to emulate a motion sensor (resets to its default state automatically)
+/// Represents the default implementation of the <see cref="IMotionSensor"/> interface
 /// </summary>
-internal class MotionSensorSimulator
-    : INotificationHandler<DeviceStateChangedDomainEvent>
+public class MotionSensorSimulator
+    : BackgroundService,
+    IMotionSensor
 {
+
     /// <summary>
-    /// Gets the <see cref="ILogger/>
+    /// Initializes a new <see cref="MotionSensorSimulator"/>
     /// </summary>
-    protected ILogger Logger { get; init; }
+    /// <param name="serviceProvider">The current <see cref="IServiceProvider"/></param>
+    /// <param name="logger">The service used to perform logging</param>
+    public MotionSensorSimulator(IServiceProvider serviceProvider, ILogger<MotionSensorSimulator> logger)
+    {
+        this.ServiceProvider = serviceProvider;
+        this.Logger = logger;
+    }
 
     /// <summary>
     /// Gets the current <see cref="IServiceProvider"/>
@@ -21,62 +28,66 @@ internal class MotionSensorSimulator
     protected IServiceProvider ServiceProvider { get; }
 
     /// <summary>
-    /// Gets the (read) <see cref="IRepository"/> used to manage the <see cref="Device"/>s
+    /// Gets the service used to perform logging
     /// </summary>
-    protected IRepository<Device, string> Devices { get; init; }
+    protected ILogger Logger { get; }
 
     /// <summary>
-    /// Gets the service used to map objects
+    /// Gets an <see cref="ConcurrentDictionary{TKey, TValue}"/> containing the states of the motion sensors mapped by id
     /// </summary>
-    protected IMapper Mapper { get; init; }
-
-    public static CancellationTokenSource? CancellationTokenSource = null;
-    private string _motionSensorIdPrefix = "motion-sensor";
+    protected ConcurrentDictionary<string, bool> SensorStates { get; } = new();
 
     /// <summary>
-    /// Initializes a <see cref="AcSimulator"/>
+    /// Gets the <see cref="MotionSensorSimulator"/>'s <see cref="System.Threading.CancellationTokenSource"/>
     /// </summary>
-    /// <param name="logger">The service used to log</param>
-    /// <param name="devices">The <see cref="IRepository"/> used to manage the <see cref="Device"/>s</param>
-    /// <param name="mediator">The service used to mediate calls</param>
-    /// <param name="mapper">The service used to map objects</param>
-    public MotionSensorSimulator(ILogger<AcSimulator> logger, IServiceProvider serviceProvider, IRepository<Device, string> devices, IMapper mapper)
+    protected CancellationTokenSource CancellationTokenSource { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets the <see cref="MotionSensorSimulator"/>'s detection <see cref="System.Threading.CancellationTokenSource"/>
+    /// </summary>
+    protected CancellationTokenSource? DetectionCancellationTokenSource { get; private set; }
+
+    /// <inheritdoc/>
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        if (logger == null) throw DomainException.ArgumentNull(nameof(logger));
-        if (devices == null) throw DomainException.ArgumentNull(nameof(devices));
-        if (mapper == null) throw DomainException.ArgumentNull(nameof(mapper));
-        this.Logger = logger;
-        this.ServiceProvider = serviceProvider;
-        this.Devices = devices;
-        this.Mapper = mapper;
+        this.CancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(stoppingToken);
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public virtual Task TriggerAsync(string sensorId, CancellationToken cancellationToken = default)
+    {
+        if(!this.SensorStates.TryGetValue(sensorId, out var isTriggered)) this.SensorStates.TryAdd(sensorId, true);
+        if (!isTriggered) _ = this.SenseMotionAsync(sensorId);
+        return Task.CompletedTask;
     }
 
     /// <summary>
-    /// Handles a <see cref="DeviceStateChangedDomainEvent"/> notification
+    /// Senses motion
     /// </summary>
-    /// <param name="notification"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    public async Task HandleAsync(DeviceStateChangedDomainEvent e, CancellationToken cancellationToken = default)
+    /// <param name="sensorId">The id of the sensor that has detected motion</param>
+    /// <returns>A new awaitable <see cref="Task"/></returns>
+    protected virtual async Task SenseMotionAsync(string sensorId)
     {
-        if (!e.AggregateId.StartsWith(this._motionSensorIdPrefix)) return;
-        var turnedOn = ((dynamic?)e.State)?.on != null && (bool)((dynamic?)e.State)?.on;
-        if (turnedOn)
+        try
         {
-            var _ = Task.Run(async () =>
-            {
-                try
-                {
-                    using var scope = this.ServiceProvider.CreateScope();
-                    var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-                    await Task.Delay(2000);
-                    await mediator.ExecuteAsync(new UpdateDeviceStateCommand(e.AggregateId, new { on = false }));
-                }
-                catch (Exception ex)
-                {
-                    this.Logger.LogError("The motion sensor experienced an exception '{ex}'", ex);
-                }
-            }, cancellationToken);
+            this.DetectionCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(this.CancellationTokenSource.Token);
+            this.SensorStates.AddOrUpdate(sensorId, true, (_, _) => true);
+            using var scope = this.ServiceProvider.CreateScope();
+            var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
+            var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+            await Task.Delay(2000);
+            await mediator.ExecuteAsync(new UpdateDeviceStateCommand(sensorId, new { on = false }));
+        }
+        catch (TaskCanceledException) { }
+        catch (Exception ex)
+        {
+            this.Logger.LogError("An error occured while sensing motion: {ex}", ex);
+        }
+        finally
+        {
+            this.SensorStates.AddOrUpdate(sensorId, false, (_, _) => false);
         }
     }
+
 }
